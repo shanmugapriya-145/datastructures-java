e6c9d2f7442d594c60a37a08191f2110d6c59beb
Shanmugapriya K <113292885+shanmugapriya-145@users.noreply.github.com>
2023-07-20 14:10:22 +0530

    Create Isprime.java

---
+++ DAY 10/Isprime.java
@@ -0,0 +1,20 @@
+//Isprime-mercer mettl
+import java.io.*;
+import java.util.*;
+class UserMainCode{
+  public int isPrime(int input1){
+         int cnt=0; 
+         for(int i=1;i<=input1;i++){
+           if(input1%i==0){
+             cnt++;
+           }
+         }
+         if(cnt==2)
+         {
+           return 2;
+         }
+         else{
+           return 1;
+         }
+  }
+}
