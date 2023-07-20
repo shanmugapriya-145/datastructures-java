4099dc423168212c6c355c54b88ecb99100c82b3
Shanmugapriya K <113292885+shanmugapriya-145@users.noreply.github.com>
2023-07-20 09:20:51 +0530

    Create N-exact multiple of M.java

---
+++ DAY 8/N-exact multiple of M.java
@@ -0,0 +1,19 @@
+//N an exact multiple of M?-Mercerc Mettl
+import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+  public int isMultiple(int input1,int input2){
+    int val=0;
+    if(input1==0 || input2==0) {
+      val=3;
+    }
+    else if((input1%input2)!=0){
+      val=1;
+    }
+    else{
+       val=2;
+    }
+    return val;
+  }
+}
