4c70c8fbbea662e2487b85a7c33c7315f37e96c1
Shanmugapriya K <113292885+shanmugapriya-145@users.noreply.github.com>
2023-07-24 11:08:57 +0530

    Create Even digits sum.java

---
+++ DAY 12/Even digits sum.java
@@ -0,0 +1,16 @@
+//Sum of even digit's in N-mercer mettl
+import java.io.*;
+import java.util.*;
+class UserMainCode{
+  public int EvenDigitsSum(int input1){
+        int r,sum=0;
+        while(input1>0){
+            r=input1%10;
+            if(r%2==0){
+                sum=sum+r;
+            }
+            input1=input1/10;
+        }
+        return sum;
+    }
+}
